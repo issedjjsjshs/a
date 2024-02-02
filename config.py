facd2f0d3d98bb2cb7de9dd21b2b56284e566faa
Aliiyp <157922994+Aliiyp@users.noreply.github.com>
2024-02-02 04:12:30 +0300

    config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ START_IMG = getenv("START_IMG", "https://telegra.ph/file/95bc0cd9d859ddc13d28a.j
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BDthon")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SourceTeo")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BDthon")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6799580948").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6755634224").split()))
 
 
-FAILED = "https://telegra.ph/file/95bc0cd9d859ddc13d28a.jpg"
+FAILED = "https://telegra.ph/-02-02-10711"
