6bee2e3fa78e09471dd91e6e3b609c53279c56c3
Aliiyp <157922994+Aliiyp@users.noreply.github.com>
2024-02-03 00:06:27 +0300

    Update inline.py

--- ShahmMusic/Helpers/inline.py
+++ ShahmMusic/Helpers/inline.py
@@ -57,7 +57,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="◈ المطور ◈", url="https://t.me/SourceTeo"
+            text="◈ المطور ◈", url="https://t.me/eTsfine"
         ),
         InlineKeyboardButton(text="◈ مالك البوت ◈", user_id=config.OWNER_ID),
     ],
